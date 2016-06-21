2f128839c84e9470d6e3ed1fdb77f39daa1f5a59
isnan.prasetya@mail.unpas.ac.id <isnan.prasetya@mail.unpas.ac.id>
2016-06-21 11:24:39 +0700

    commit sebelum di edit

---
+++ src/tugas2/NilaiArray.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package tugas2;
+
+/**
+ *
+ * @author Isnan Prasetya
+ */
+public class NilaiArray {
+    public static void main(String[] args) {
+
+	int nilai[];
+	nilai = new int[5];
+	int i;
+		
+	nilai[1] = 8;
+	nilai[2] = 2;
+	nilai[3] = 10;
+	nilai[4] = 3;
+        
+	for( i=0; i<5; i++) {
+            System.out.println("Nilai["+i+"] = "+nilai[i]);		
+	}
+    }   
+}
